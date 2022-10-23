f4e4e2a8b1af9e6d5319c0a2b0853c2030693b96
Emil Vonck <emil.vonck@gmail.com>
2022-10-23 21:53:47 +0200

    more tshooring

--- plugins/inventory/inventory.py
+++ plugins/inventory/inventory.py
@@ -62,7 +62,7 @@ class InventoryModule(BaseInventoryPlugin):
             "ansible_host": self.extract_connecting_from,
             "serial": self.extract_serial,
             "status": self.extract_status,
-            "ansible_distribution": self.extract_os_distribution,
+            # "ansible_distribution": self.extract_os_distribution,
             # "ansible_distribution_version": self.extract_os_version,
         }
 
